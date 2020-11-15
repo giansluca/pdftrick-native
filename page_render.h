296083b7a1ec37959a4899ac79ba50578e64223b
gians <giansluca@gmail.com>
2020-11-15 23:42:55 +0100

    first commit

---
+++ page_render.h
@@ -0,0 +1,8 @@
+
+#ifndef PDFTRICK_NATIVE_PAGE_RENDER_H
+#define PDFTRICK_NATIVE_PAGE_RENDER_H
+
+void render(const char *filename, int zoom, int rotation, char *img_path,
+            int pagenumber);
+
+#endif

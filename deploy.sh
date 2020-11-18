3159ce68dbd9be8ab4bb9f3c4ceb69e96ef216fb
gians <giansluca@gmail.com>
2020-11-18 21:20:38 +0100

    travis update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
-  echo "deploy mac done"
+  export SSHPASS=$DEPLOY_PASS
+  sshpass -e scp ./dist/mac/release/libpdftrick_native_1.7a_64.jnilib $DEPLOY_USER@$DEPLOY_HOST:/home/gians/test-deploy
 else
-  echo "deploy win done"
+  pscp -pw $DEPLOY_PASS ./dist/win/release/libpdftrick_native_1.7a_64.dll $DEPLOY_USER@$DEPLOY_HOST:/home/gians/test-deploy
 fi
 

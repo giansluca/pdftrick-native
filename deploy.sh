cfe8204ef97093dd4b7a68b4d3875666e2e13456
gians <giansluca@gmail.com>
2020-11-18 22:26:18 +0100

    travis config update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
-  ls
   export SSHPASS=$DEPLOY_PASS
   sshpass -e scp -o stricthostkeychecking=no ./dist/mac/release/libpdftrick_native_1.7a_64.jnilib $DEPLOY_USER@$DEPLOY_HOST:/home/gians/test-deploy
 else

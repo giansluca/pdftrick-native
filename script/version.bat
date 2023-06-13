2acb740804bbfe693333621f8bc312a75cf19501
gians <giansluca@gmail.com>
2023-06-13 17:35:53 +0200

    test pipeline

--- script/version.bat
+++ script/version.bat
@@ -1,3 +1,2 @@
-::@set @VERSION="1.0.3"
 @set /p @VERSION=< "./script/version.txt"  
 @echo | set /p=%@VERSION%
--- script/version.sh
+++ script/version.sh
@@ -1,3 +1,2 @@
-#VERSION="1.0.3"
-VERSION=`head -n 1 script/version.txt`
+VERSION=`head -n 1 ./script/version.txt`
 printf $VERSION
